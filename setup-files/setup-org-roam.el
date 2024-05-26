3f840c05761eaf4c8d6df4fdec493f36f2cac5a2
minh-p <minhisverynormal@gmail.com>
2024-05-26 14:26:41 -0700

    Org Roam Keybinds

--- setup-files/setup-org-roam.el
+++ setup-files/setup-org-roam.el
@@ -1,4 +1,7 @@
-(use-package org-roam)
+(use-package org-roam
+  :bind
+  ("C-c o n" . org-roam-node-find)
+  ("C-c o d" . org-roam-dailies-goto-date))
 
 (setq org-roam-directory (file-truename "~/org-roam"))
 (setq find-file-visit-truename t)
