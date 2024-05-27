a670bae752cc2f29ff5067c5e206b68d1231a97e
minh-p <minhisverynormal@gmail.com>
2024-05-26 17:47:24 -0700

    consult-org-heading bind

--- setup-files/setup-completion.el
+++ setup-files/setup-completion.el
@@ -38,6 +38,7 @@
          ("C-c k" . consult-kmacro)
          ("C-c m" . consult-man)
          ("C-c i" . consult-info)
+         ("C-c o h" . consult-org-heading)
          ([remap Info-search] . consult-info)
          ;; C-x bindings in `ctl-x-map'
          ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
